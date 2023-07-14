49928b725e24714d1c0e18799e33ed0ee54ebbc2
Josh Justice <me@codingitwrong.com>
2023-07-14 19:57:39 -0400

    Update GitHub URL

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -78,7 +78,7 @@ const config = {
           },
           {to: '/blog', label: 'Blog', position: 'left'},
           {
-            href: 'https://github.com/facebook/docusaurus',
+            href: 'https://github.com/CodingItWrong?tab=repositories&q=riverbed&type=&language=&sort=',
             label: 'GitHub',
             position: 'right',
           },
